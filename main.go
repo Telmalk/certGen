cfd492991bd6ae6a06a04f9370017399fa0d7837
Clément Haller <clement.haller95@hotmail.fr>
2020-06-23 16:42:03 +0200

    csv Parser

--- main.go
+++ main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"training.go/gencert/cert"
+	"training.go/gencert/html"
 	"training.go/gencert/pdf"
 )
 
 func main()  {
-	c, err := cert.New("GoLang", "Clement Haller", "2020-06-16")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
-	var saver cert.Saver
-	saver, err =  pdf.New("output")
+	outputType := flag.String("type", "pdf", "Output type of the certificate")
+	flag.Parse()
+	/*
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	err = saver.Save(*c)
+	*/
+	var saver cert.Saver
+	var err error
+	switch *outputType {
+	case "html":
+		saver, err =  html.New("output")
+	case "pdf":
+		saver, err =  pdf.New("output")
+	default:
+		fmt.Printf("Unknow output type. got'%v'\n", *outputType)
+	}
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Could not create generator: %v", err)
 		os.Exit(1)
+	}
+	// c, err := cert.New("GoLang", "Boby", "2020-06-16")
+	certs, err := cert.ParseCSV("student.csv")
+	for _, c := range certs {
+		err := saver.Save(*c)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
